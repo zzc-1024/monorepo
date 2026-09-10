f177a2888bd0a95653c2da0c2912f07a87a22e08
周子程 <1977309465@qq.com>
2026-09-10 18:19:22 +0800

    🚧 未完待续: 计划将对话组件引入 web repo 中，但是尚未完工

--- apps/web/src/App.vue
+++ apps/web/src/App.vue
@@ -1,15 +1,11 @@
 <script setup lang="ts">
 import { ChatPanel } from '@repo/components';
 import { t } from '@repo/i18n';
+
 </script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
-  <ChatPanel></ChatPanel>
+  <ChatPanel style="height: 100dvh;"></ChatPanel>
   {{ t('greeting', { name: 'John' }) }}
 </template>
 
