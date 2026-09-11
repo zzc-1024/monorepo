53ae12dfb44e2d182af06d29ed68e3d6f4caeed4
周子程 <1977309465@qq.com>
2026-09-11 22:51:10 +0800

    🎨 打扫屋子: 格式化代码

--- apps/web/src/App.vue
+++ apps/web/src/App.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
 import { ChatPanel } from '@repo/components';
 import { t } from '@repo/i18n';
-
 </script>
 
 <template>
-  <ChatPanel style="height: 100dvh;"></ChatPanel>
+  <ChatPanel style="height: 100dvh"></ChatPanel>
   {{ t('greeting', { name: 'John' }) }}
 </template>
 
