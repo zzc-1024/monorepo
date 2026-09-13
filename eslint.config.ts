f92899311ebead3a326d15efdc8de31697b725a1
周子程 <1977309465@qq.com>
2026-09-14 00:20:22 +0800

    🚨 防患未然: 新增若干条 lint 规则

--- eslint.config.ts
+++ eslint.config.ts
@@ -17,10 +17,40 @@ export default defineConfigWithVueTs(
 
   globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
 
-  ...pluginVue.configs['flat/essential'],
+  // ...pluginVue.configs['flat/essential'],
+  ...pluginVue.configs['flat/recommended-error'],
   vueTsConfigs.recommended,
 
   ...pluginOxlint.buildFromOxlintConfigFile('.oxlintrc.json'),
 
   skipFormatting,
+
+  // 自定义的规则集
+  {
+    rules: {
+      'vue/define-emits-declaration': ['error', 'type-literal'],
+      'vue/define-props-declaration': ['error'],
+      'vue/no-undef-components': [
+        'error',
+        {
+          ignorePatterns: [],
+        },
+      ],
+      'vue/no-unused-refs': ['error'],
+      'vue/no-use-v-else-with-v-for': ['error'],
+      'vue/prefer-use-template-ref': ['error'],
+      'vue/require-macro-variable-name': [
+        'error',
+        {
+          defineProps: 'props',
+          defineEmits: 'emit',
+          defineSlots: 'slots',
+          useSlots: 'slots',
+          useAttrs: 'attrs',
+        },
+      ],
+    },
+    'vue/require-prop-comment': ['error'],
+    'vue/require-typed-ref': ['error'],
+  },
 );
