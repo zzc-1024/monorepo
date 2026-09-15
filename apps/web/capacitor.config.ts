4abf5d7acd1e104a44e9049b653efc3f432e84d1
周子程 <1977309465@qq.com>
2026-09-15 14:03:37 +0800

    :art: 打扫屋子: 格式化代码

--- apps/web/capacitor.config.ts
+++ apps/web/capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'unirepo',
-  webDir: 'dist'
+  webDir: 'dist',
 };
 
 export default config;
